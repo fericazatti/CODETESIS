401cd614c54d9e9464614e86ba2261edfc2838d5
ferIcazatti <fericazatti@gmail.com>
2023-04-19 01:35:26 -0300

    Genereat all combination of hfo detector params and others

--- testhfo.py
+++ testhfo.py
@@ -23,30 +23,56 @@ from util.plots.hfo_plots import *
 
 from skimage.filters import threshold_otsu
 import pandas as pd
+import xarray as xr
+
+from itertools import product
+
+
+def alghoritm_params(lower_freq, upper_freq, band_width, window_min, window_max, window_step):
+    """
+    objective: create array with differents algohortim configuration.
+    The objective of the function is to generate an array with different algorithm configurations for the detection of high frequency oscillations (HFOs) in EEG data. The function takes as input several parameters related to the frequency bands and window sizes to be used in the HFO detection algorithm.
+    
+    Outpout: 
+    - all_comb: an array containing tuples representing all the different algorithm configurations to be tested in the HFO detection process.
+
+    """
+    band_width_2 = upper_freq - lower_freq
+    filter_bands = []
+    filter_bands.append((lower_freq, upper_freq))
+
+    for i in range(round(band_width_2 / (band_width / 2)) - 1):
+        if lower_freq + 30 * i + band_width == upper_freq + 1:
+            band_width = band_width - 1
+        filter_bands.append(tuple([lower_freq + 30 * i, lower_freq + 30 * i + band_width]))   
+
+    all_combinations = product(filter_bands, range(window_min, window_max + 1, window_step))
+
+    return all_combinations
+        
+    # end def
+# end def
 
 #########import data
+raw = mne.io.read_raw_edf('subjects/sub-HUP160/sub-HUP160_ses-presurgery_ieeg_sub-HUP160_ses-presurgery_task-interictal_acq-seeg_run-01_ieeg.edf', preload = True) 
+ch_info = pd.read_csv('subjects/sub-HUP160/sub-HUP160_ses-presurgery_ieeg_sub-HUP160_ses-presurgery_task-interictal_acq-seeg_run-01_channels.tsv', sep = '\t')
 
-# raw = mne.io.read_raw_edf('subjects/alemanno/ALEMANO_34_FEDERICO.edf',preload = True)
-#raw = mne.io.read_raw_edf('subjects/sub-HUP160/sub-HUP160_ses-presurgery_ieeg_sub-HUP160_ses-presurgery_task-interictal_acq-seeg_run-01_ieeg.edf', preload = True) 
-raw = mne.io.read_raw_edf('/home/proyectoepilepsia/Documents/software/CODETESIS/subjects/sub-HUP060/sub-HUP060_ses-presurgery_task-interictal_acq-seeg_run-02_ieeg.edf',
-                            preload=True)
-# raw = mne.io.read_raw_edf('/home/proyectoepilepsia/Documents/Pruebas/CodeTesis/subjects/sub-HUP160/sub-HUP160_ses-presurgery_task-interictal_acq-ecog_run-01_ieeg.edf',
-#                            preload=True)
-ch_info = pd.read_csv('subjects/sub-HUP060/sub-HUP060_ses-presurgery_task-interictal_acq-seeg_run-02_channels.tsv', sep = '\t')
-# ch_localization = pd.read_csv('subjects/sub-HUP160/sub-HUP160_ses-presurgery_ieeg_sub-HUP160_ses-presurgery_acq-ecog_space-fsaverage_electrodes.tsv', sep = '\t')
-
-### Query bads and surgical channels
+# %% Query bads and surgical channels
 ch_bads = ch_info.loc[ch_info['status'] == 'bad']['name']
 ch_surgical = ch_info.loc[ch_info['status_description'] == 'soz']['name']
 
-#### delete from raw file bad channels
+# %% delete from raw file bad channels
 raw.drop_channels(ch_bads)
 
+# %% comvert to bipolar reference
 raw = convert_to_bipolar(raw)
+
+for i in range(len(raws)):
+    raws[i] = convert_to_bipolar(raws[i])
 
 # Set Key Word Arguments for the Line Length Detector and generate the class object
 kwargs = {
-    'filter_band': (80, 249), # (l_freq, h_freq)
+    'filter_band': (80, 120), # (l_freq, h_freq)
     'threshold': 3, # Number of st. deviations
     'win_size': 100, # Sliding window size in samples
     'overlap': 0.25, # Fraction of window overlap [0, 1]
@@ -65,6 +91,7 @@ ll_detector = LineLengthDetector(**kwargs)
 # ll_hfo_df = ll_detector.df_
 
 # Detect HFOs in the raw data using the RMSDetector method.
+
 rms_detector = rms_detector.fit(raw)
 
 ### Outpout detector to dataframe pandas
@@ -80,3 +107,55 @@ hfo_dist_df = plot_events_hfo_2(rms_hfo_df['channels'], ch_info, raw._last_time)
 bar_chart(hfo_dist_df)
 
 histogram_hfo(hfo_dist_df)
+
+for raw in raws:
+    rms_detector = rms_detector.fit(raw)
+
+    ### Outpout detector to dataframe pandas
+    rms_hfo_df = rms_detector.df_
+
+    # plot_events_hfo(ll_hfo_df['channels']) #la funcion recibe la serie de canales en los eventos encontrados
+    plot_events_hfo(rms_hfo_df['channels'])
+
+    # plot_events_hfo_2(ll_hfo_df['channels'], ch_info)
+    # hfo_dist_df = plot_events_hfo_2(rms_hfo_df['channels'], ch_info, raw._last_time)
+
+    ###### Generación del histograma y posterior umbralización
+    # bar_chart(hfo_dist_df)
+
+    # histogram_hfo(hfo_dist_df)
+
+kwargs = {
+    'lower_freq':   40,
+    'upper_freq':   249,
+    'band_width':   60,
+    'window_min':   50,
+    'window_max':   150,
+    'window_step':  20
+}
+
+# definir las dimensiones y sus etiquetas
+algorithms_config = []
+channels = ['channel1', 'channel2', 'channel3', 'channel4']
+values_coords = ['counts rate', 'status', 'color']
+
+# crear el xarray con las dimensiones y valores
+data_array = xr.DataArray(coords=[algorithms_config, channels, values_coords], dims=['algorithms_config', 'channels', 'values'])
+
+print(data_array)
+for param_combine in alghoritm_params(**kwargs):
+    
+    new_algorithm_config = ['config1']
+    data_array = data_array.reindex(algorithms_config = new_algorithm_config)
+
+    
+    kwargs = {
+        'filter_band': param_combine[0], # (l_freq, h_freq)
+        'threshold': 3, # Number of st. deviations
+        'win_size': param_combine[1], # Sliding window size in samples
+        'overlap': 0.25, # Fraction of window overlap [0, 1]
+        'hfo_name': "ripple"
+    }
+    
+    rms_detector = RMSDetector(**kwargs) 
+    rms_detector = rms_detector.fit(raw)
