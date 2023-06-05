9bf31e947fbbe89f37772c11565c2be778ae33aa
ferIcazatti <fericazatti@gmail.com>
2023-06-05 11:59:36 -0300

    analysis.py advances

--- analysis.py
+++ analysis.py
@@ -149,7 +149,7 @@ for i, element in enumerate(ds.coords["algorithm_params"].values):
     df_process = df_process.join(df_drop['hrr']).rename(columns={'hrr': f'hfo_detect_C{i+1}'})
     df_decode_detect = df_decode_detect.append({'name': f'hfo_detect_C{i+1}', 'combination': element}, ignore_index=True)
 
-   # df_drop[['hrr','outcome']].plot.scatter(x='hrr',y='outcome').set_title(element)
+    # df_drop[['hrr','outcome']].plot.scatter(x='hrr',y='outcome').set_title(element)
 
 
 
@@ -220,11 +220,11 @@ df_process = pd.DataFrame(dictonary)
 df_process = df_process.set_index('id')
 # %% get hrr results vs outcome 
 
-df = ds.sel(algorithm_params = 'bw-(130, 190)_ww-90').to_pandas().T
+df = ds.sel(algorithm_params = 'bw-(160, 220)_ww-110').to_pandas().T
 df_drop = df.drop("algorithm_params")
 df_drop[['hrr','outcome']].plot.scatter(x='hrr',y='outcome').set_title('bw-(130, 190)_ww-90')
 
-# %% optimizacion de parametors de regresión logistica
+# %% optimizacion de parametros de regresión logistica
 
 train = df_drop
 # definiendo input y output
@@ -256,4 +256,106 @@ plt.plot(x, y.reshape(1000), '-r')
 plt.ylim(0,train['outcome'].max()*1.1)
 # plt.grid()
 plt.show()
+
+# %% global analysis of logistic regression
+df_results = {
+    'combination_name': [],
+    'band_width':[],
+    'window_width':[],
+    'accuracy': [],    
+    'f1-score': [],
+    'AUC': []
+}
+for value in df_decode_detect.iterrows():
+    value = value[1]
+    
+    df = ds.sel(algorithm_params = value['combination']).to_pandas().T
+    df_drop = df.drop("algorithm_params")
+    
+    train = df_drop
+    # definiendo input y output
+    X_train = np.array(train['hrr']).reshape((-1, 1))
+    Y_train = np.array(train['outcome'])
+    Y_train = Y_train.astype(int)
+
+    # creando modelo
+    model = LogisticRegression()
+    model.fit(X_train, Y_train)
+
+    Y_pred = model.predict(X_train)
+    report_dict = classification_report(Y_train, Y_pred, output_dict=True)
+    
+    #AUC calculo
+    y_pred_proba = model.predict_proba(X_train)[:, 1]
+    fpr, tpr, thresholds = roc_curve(Y_train, y_pred_proba)
+    auc = roc_auc_score(Y_train, y_pred_proba)   
+        
+    #agregar valores
+    df_results['combination_name'].append(value['name'])
+    df_results['band_width'].append(None)
+    df_results['window_width'].append(None)
+    df_results['accuracy'].append(report_dict['accuracy'])
+    df_results['f1-score'].append(report_dict['1']['f1-score'])
+    df_results['AUC'].append(auc)
+    
+    df_results = pd.DataFrame(df_results)
+# %% Prueba plot de sickit learn 
+
+
+producto = df_results['accuracy'] * df_results['f1-score']
+delete_repeat = set(producto)
+ordered = sorted(list(delete_repeat))
+maximos = ordered[-3:]
+indices_maximo = []
+for maximo in maximos:
+    indices_maximo.append(np.where(producto == maximo)[0].tolist())
+
+for row in indices_maximo:
+    for maximo in row:
+        
+        name = df_results.iloc[maximo]['combination_name']
+        combination = df_decode_detect.loc[df_decode_detect['name'] == name]['combination']
+        
+        df = ds.sel(algorithm_params = combination.iloc[0]).to_pandas().T
+        df_drop = df.drop("algorithm_params")
+        train = df_drop
+
+        # Fit the classifier
+        clf = LogisticRegression(C=1e5)
+        clf.fit(X_train, Y_train)
+
+        # and plot the result
+        plt.figure(1, figsize=(6, 3))
+        plt.clf()
+        plt.scatter(X_train.ravel(), Y_train, label="example data", color="blue", zorder=20)
+        X_test = np.linspace(-0.5, 1, 300)
+
+        loss = expit(X_test * clf.coef_ + clf.intercept_).ravel()
+        plt.plot(X_test, loss, label="Logistic Regression Model", color="red", linewidth=3)
+
+        # ols = LinearRegression()
+        # ols.fit(X_train, Y_train)
+        # plt.plot(
+        #     X_test,
+        #     ols.coef_ * X_test + ols.intercept_,
+        #     label="Linear Regression Model",
+        #     linewidth=1,
+        # )
+        plt.axhline(0.5, color=".5")
+
+        plt.ylabel("outcome")
+        plt.xlabel("hrr")
+        plt.xticks([0, 0.25, 0.5, 0,75])
+        plt.yticks([0, 0.5, 1])
+        plt.ylim(-0.25, 1.25)
+        plt.xlim(-0.2, 0.75)
+        plt.legend(
+            
+            fontsize="small",
+        )
+        plt.tight_layout()
+        plt.title( combination.iloc[0])
+        plt.show()
+
 # %%
+
